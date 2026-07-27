82c0b56c2797e1eb92dfa50955022dcdf79438d9
nikitosabrikos <nikitosabrikos985@gmail.com>
2026-07-27 13:36:12 +0300

    astro config

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,4 +1,6 @@
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
-  site: 'https://<jaydenwhitehereboyz>.github.io',
+  site: 'https://jaydenwhitehereboyz.github.io',
   base: '/saas-revenue-retention-analytics',
 });
